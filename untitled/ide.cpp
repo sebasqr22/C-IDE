1ff9fe081b181bc798bd4acde9248e611573d086
sebasqr22 <sebastianqr.2208@gmail.com>
2021-04-14 10:16:08 -0600

    Se agrega el adapter
    Co-authored-by: Kenichi Hayawaka <kenichih48@yahoo.com>

--- untitled/ide.cpp
+++ untitled/ide.cpp
@@ -15,61 +15,40 @@ ide::~ide()
     delete ui;
 }
 
-void ide::on_runBut_clicked()
+void ide::on_runBut_clicked()//basicamente esto es un adapter
 {
     QString tmp;
-    int cantidadPuntoComa;
 
     tmp = ui->editor->toPlainText();
     qInfo() << tmp;
     tmp.remove("\n");
     qInfo() << tmp;
-    cantidadPuntoComa = tmp.count(";");
 
     QString temp2 = "";
     QStringList listaInfo;
     int largo = tmp.size();
 
-    for(int i=0; i > largo; i++){
-        if(tmp[i] == ";"){
-            qInfo()<<"entrando al if";
+    for(int i=0; i < largo; i++){
+        if(tmp[i] == "{"){
+            QStringList aux;
+            int largo2 = i;
+            for(largo2; tmp[largo2] != "}"; largo2++){
+                temp2.append(tmp[largo2]);
+            }
+            temp2.append("}");
+            i = largo2;
+            aux << temp2;
+            listaInfo << aux;
+            temp2 = "";
+        }
+        else if(tmp[i] != ";"){
+            temp2.append(tmp[i]);
+        }
+        else{
             listaInfo << temp2;
             temp2 = "";
-            qInfo()<<temp2;
         }
-        else{
-            qInfo()<<"ENtrando al else";
-            temp2.append(tmp[i]);        }
-            qInfo()<<temp2;
+
     }
     qInfo() << listaInfo;
-
-    // Despues se crea el array con la info correspondiente
-
-
-
-/*
-    QString arr[cantidadPuntoComa];
-    QString tmp2 = "";
-
-    qInfo() << "Variables creadas";
-
-    for(int i = 0; i == tmp.size(); i++){
-        if(tmp[i] != ";"){
-            tmp2 += tmp[i];
-        }
-        else{
-            arr[contador] = tmp2;
-            tmp2 = "";
-            contador++;
-        }
-    }
-    qInfo() << "Termino primer for";
-
-    for(int a=0; a==cantidadPuntoComa; a++){
-        qInfo() << "Posicion: " << a << "" << arr[a];
-    }
-
-    qInfo() << "Termino segundo for";
-*/
 }
