060dd938848394e15e5b4bcca7bc1e7326cabdbc
kenichih48 <kenichih48@yahoo.com>
2021-04-18 21:56:06 -0600

    Inicios del server y pruebas

---
+++ untitled/mserver.cpp
@@ -0,0 +1,95 @@
+//parámetros el puerto en que escucha y el tamaño en bytes de la memoria total
+
+//un malloc, cuando recibe una petición de C!, maneja offsets para determinar posicion de cada 
+//variable C! en la memoria real
+
+//escucha peticiones mandados en json, indica tipo de datos, nombre de variable, 
+//tamaño de la variable por reserver
+
+//server maneja la memoria
+
+//lleva conteo de referencias cada cierto tiempo maneja garbage collector que elimina espacios 
+//de memoria no referenciados
+/*
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+int main(int port, size_t size){ //size en bytes
+    int *ptr;
+    ptr = (int*) malloc(size);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int setsock(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
+    int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+    int listen(int sockfd, int backlog);
+    int new_socket = accept(int sockfd, struct sockaddr *addr, socklen_t addrlen);
+    //ptr + i; offset
+    
+    return 0;
+}*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include "json.hpp"
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "hi";
+       
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+       
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+       
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    return 0;
+}
