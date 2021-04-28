db5133970cd921578829b2d7fccbe78b932cd848
sebasqr22 <sebastianqr.2208@gmail.com>
2021-04-27 23:55:28 -0600

    Diagrama de clases
    Co-authored-by: Kenichi Hayakawa <kenichih48@yahoo.com

---
+++ cmake-build-debug/CMakeFiles/C_IDE.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_IDE.dir/build-untitled-Desktop-Debug/moc_ide.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/Cliente.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/creadorListas.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/ide.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/logger.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/main.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/mserver.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/operaciones.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/operacionesEstructs.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/pruebas.cpp.o"
+  "CMakeFiles/C_IDE.dir/untitled/test.cpp.o"
+  "C_IDE"
+  "C_IDE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_IDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
